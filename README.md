b17e4a6ac35ba29baf80fd1db8b0871269b8e70e
Scott Francis <scottfrancis@Scotts-MacBook-Air.local>
2017-04-12 14:29:07 -0700

    ng-izing

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ Generate new creds as needed
 
 540312807168-vc7q1q8le7ln4s7ku9l8950d4dv6mebp.apps.googleusercontent.com
 89H9XJ-SVW1003gK4FcHM4Si
+
+1esqY9xgLO97eeICqQoC7AwIzgXtEvfEjpd7QbQiU6LU
--- app.js
+++ app.js
@@ -1,53 +1,100 @@
 // browser-sync start --server --directory --files "**/*"
 (function() {
-'use strict';
-
-  angular.module('ShoppingListCheckOff', [])
-    .controller('ToBuyController', ToBuyController)
-    .controller('AlreadyBoughtController', AlreadyBoughtController)
-    .service('ShoppingListCheckOffService', ShoppingListCheckOffService)
+'use strict'
 
 
-  // ShoppingListCheckOffService.$inject = ['$scope']
-  function ShoppingListCheckOffService() {
+  angular.module('DocComments', [])
+      // localhost
+    // .constant('CLIENT_ID', "540312807168-vc7q1q8le7ln4s7ku9l8950d4dv6mebp.apps.googleusercontent.com")
+    // .constant('DISCOVERY_DOCS', ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"])
+    // .constant('SCOPES', 'https://www.googleapis.com/auth/drive')
+    .controller('DocController', DocController)
+    .controller('ActionItemController', ActionItemController)
+    // .service('GapiService', GapiService)
+    .service('FileCommentService', FileCommentService)
+
+
+  // FileCommentService.$inject = ['$scope']
+  FileCommentService.$inject = []
+  function FileCommentService() {
     var checker = this
 
-    var buyList = [ { name: "cookies", quantity: 10 },
-          { name: "beers", quantity: 12 },
-          { name: "pizzas", quantity: 4 },
-          { name:  "puppies", quantity: 3 },
-          { name:  "apples", quantity: 200 }]
-    var boughtList = []
+    var isSignedIn = false
+
+    var fileList = []
+    var actionList = []
 
 
-    checker.getBuyList = function() {
-      return buyList
+    checker.getFileList = function() {
+      return fileList
     }
 
-    checker.getBoughtList = function() {
-      return boughtList
+    checker.getActionList = function() {
+      return actionList
     }
 
-    checker.buyItem = function(idx) {
-      boughtList.push(buyList.splice(idx, 1)[0])
+    checker.getSignedIn = function() {
+      return isSignedIn
+    }
+
+    checker.updateSigninStatus = function(status) {
+      isSignedIn = status
+    }
+  }
+
+  // GapiService.$inject = ['CLIENT_ID', 'DISCOVERY_DOCS', 'SCOPES']
+  // function GapiService(clientId, discoveryDocs, scopes) {
+  //   this.isInitialized = false
+  //
+  //   this.initGapi = function(postInit) {
+  //     // var gapi = $rootScope.gapi
+  //     if (Gapi !== undefined) {
+  //       var client = Gapi.client
+  //
+  //       if (client !== undefined) {
+  //         client.init({
+  //           discoveryDocs: discoveryDocs,
+  //           clientId: clientId,
+  //           scope: scopes
+  //         }).then(function () {
+  //           this.isInitialized = true
+  //
+  //           // Listen for sign-in state changes.
+  //          gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus)
+  //
+  //           // Handle the initial sign-in state.
+  //          updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get())
+  //          authorizeButton.onclick = handleAuthClick
+  //          signoutButton.onclick = handleSignoutClick
+  //
+  //           postInit()
+  //         })
+  //       }
+  //     }
+  //   }
+  //
+  //   // this.initGapi()
+  // }
+
+  DocController.$inject = ['FileCommentService']
+  function DocController(fileCommentService) {
+    var docs = this
+    docs.itemList = fileCommentService.getFileList()
+
+    // var postInit = function() {
+    //   // ... whatever...
+    //   console.log('postInit')
+    // }
+
+    docs.getActions = function(i) {
+      fileCommentService.getActionList(i)
     }
   }
 
 
-  ToBuyController.$inject = [ 'ShoppingListCheckOffService' ]
-  function ToBuyController(ShoppingListCheckOffService) {
-    var buyer = this
-    buyer.itemList = ShoppingListCheckOffService.getBuyList()
-
-    buyer.buyItem = function(i) {
-      ShoppingListCheckOffService.buyItem(i)
-    }
-  }
-
-
-  AlreadyBoughtController.$inject = ['ShoppingListCheckOffService']
-  function AlreadyBoughtController(ShoppingListCheckOffService) {
-    var bought = this
-    bought.itemList = ShoppingListCheckOffService.getBoughtList()
+  ActionItemController.$inject = ['FileCommentService']
+  function ActionItemController(FileCommentService) {
+    var actions = this
+    actions.itemList = FileCommentService.getActionList()
   }
 })()
